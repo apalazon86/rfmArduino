f6fc67f1d7857f297c21d639cdc7eea330cf7155
Alfonso Palazón <alfonsopalazon86@hotmail.com>
2016-05-27 18:27:05 +0200

    small fix

--- rfid_multiple_read/rfid_multiple_read/rfid_multiple_read.ino
+++ rfid_multiple_read/rfid_multiple_read/rfid_multiple_read.ino
@@ -11,14 +11,9 @@ MFRC522 rfid(SS_PIN, RST_PIN);
 //Creacion de struct key que almacenará la información de una tarjeta
 MFRC522::MIFARE_Key key;
 
-//Cadenas para almacenar los NUIDs ya leidos y el actual
-String oldNUIDS="";
-String NUID="";
+//Cadenas para almacenar el NUID
 
-//Lectura totales
-int total=0;
-//Lecturas simultaneas
-int lecturasSimultaneas=0;
+String NUID="";
 
 void setup() { 
   //Inicializacion del puerto serie
@@ -32,7 +27,7 @@ void setup() {
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
-
+  Serial.println("Esperando tarjetas");
 }
  
 void loop() {
@@ -46,45 +41,17 @@ void loop() {
   if ( ! rfid.PICC_ReadCardSerial())
     return;
 
+  NUID="";
+  
   //Se almacena la NUID
   for (byte i = 0; i < 4; i++) {
     NUID+= rfid.uid.uidByte[i];
-  }
-  
+  }  
   //Se muestra la NUID por el monitor serie
-  Serial.println(NUID);
+  Serial.println("Se ha leido la tarjeta con NUID: "+NUID);
   
-  //Se le concatena una almohadilla
-  NUID+="#";
-
-  //Si la tarjeta ya ha sido leida
-  if(StringContains(oldNUIDS,NUID)>=0){
-    Serial.println("La tarjeta ya se ha leido");
-  }
-  else{
-    Serial.println("Tarjeta nueva leida");
-    oldNUIDS+=NUID;
-    total++;
-  }
   //Se detiene la lectura anterior
   rfid.PICC_HaltA();
   rfid.PCD_StopCrypto1();
 
-  //Se inicializa a vacio NUID para almacenar la siguiente lectura
-  NUID="";
-  Serial.println("Lecturas totales");
-  Serial.println(total);
 }
-
-//Funcion para saber si una cadena esta contenida en otra
-int StringContains(String s, String search) {
-    int max = s.length() - search.length();
-    int lgsearch = search.length();
-
-    for (int i = 0; i <= max; i++) {
-        if (s.substring(i, i + lgsearch) == search) return i;
-    }
-
- return -1;
-}
-
